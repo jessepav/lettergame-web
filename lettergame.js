f13e1cb344ad17ad345aa68ca09b266430530577
Jesse Pavel <jpavel@alum.mit.edu>
2020-04-01 18:38:25 -0400

    Remove button, add Ctrl combination

--- lettergame.js
+++ lettergame.js
@@ -1,21 +1,18 @@
 let holder = document.getElementById("holder");
-let button = document.getElementById("all-purpose-button");
 let background = document.getElementById("background");
 let letterWrapper = document.getElementById("letter-wrapper");
 let letter = null;
 
-const bw = button.offsetWidth, bh = button.offsetHeight;
+
 let cw, ch;  // dimensions of our holder
 
 function layoutElements() {
   let w = window.innerWidth;
   let h = window.innerHeight;
-  button.style.left = `${w / 2 - bw / 2}px`;
-  button.style.bottom = '15px';
   holder.style.top = '15px';
   holder.style.left = '15px';
   holder.style.width = String(w - 30) + 'px';
-  holder.style.height = `${h - bh - 45}px`;
+  holder.style.height = `${h - 30}px`;
   cw = holder.clientWidth;
   ch = holder.clientHeight;
   if (letter)
@@ -25,18 +22,22 @@ function layoutElements() {
 window.addEventListener('resize', layoutElements);
 window.addEventListener('load', layoutElements);
 
-button.addEventListener('click', function() {
+function toggleAnimation() {
   if (background.style.animationPlayState == 'running')
     background.style.animationPlayState = 'paused';
   else
     background.style.animationPlayState = 'running';
-});
+}
 
 const letterRegexp = /^[A-Za-z0-9]$/;
 
 document.addEventListener('keydown', keyEv => {
   if (keyEv.repeat)
     return;
+  if (keyEv.key == 'a' && keyEv.ctrlKey) {
+    toggleAnimation();
+    return;
+  }
   if (letterRegexp.test(keyEv.key)) {
     if (!letter) {
       letter = document.createElement("div");
