fe8027c78eb29c691e994ba50ca8f8ea7630f31c
Jesse Pavel <jpavel@alum.mit.edu>
2020-04-01 18:44:22 -0400

    Add fullscreen support

--- lettergame.js
+++ lettergame.js
@@ -9,10 +9,11 @@ let cw, ch;  // dimensions of our holder
 function layoutElements() {
   let w = window.innerWidth;
   let h = window.innerHeight;
-  holder.style.top = '15px';
-  holder.style.left = '15px';
-  holder.style.width = String(w - 30) + 'px';
-  holder.style.height = `${h - 30}px`;
+  let margin = document.fullscreenElement ? 0 : 15;
+  holder.style.top = margin + 'px';
+  holder.style.left = margin + 'px';
+  holder.style.width = String(w - margin*2) + 'px';
+  holder.style.height = `${h - margin*2}px`;
   cw = holder.clientWidth;
   ch = holder.clientHeight;
   if (letter)
@@ -29,15 +30,28 @@ function toggleAnimation() {
     background.style.animationPlayState = 'running';
 }
 
+function toggleFullScreen() {
+  if (document.fullscreenElement)
+    document.exitFullscreen();
+  else
+    holder.requestFullscreen();
+}
+
 const letterRegexp = /^[A-Za-z0-9]$/;
 
 document.addEventListener('keydown', keyEv => {
   if (keyEv.repeat)
     return;
   if (keyEv.key == 'a' && keyEv.ctrlKey) {
+    keyEv.preventDefault();
     toggleAnimation();
     return;
+  } else if (keyEv.key == 'f' && keyEv.ctrlKey) {
+    keyEv.preventDefault();
+    toggleFullScreen();
+    return;
   }
+
   if (letterRegexp.test(keyEv.key)) {
     if (!letter) {
       letter = document.createElement("div");
